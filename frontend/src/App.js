56f63b1f6f04103d0c640183d45e751a7e056747
nijaskhan <nijaskhan93@gmail.com>
2023-07-07 15:25:20 +0530

    git initialized for video-call-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,168 @@
+import React, { useEffect, useRef, useState } from 'react'
+import './App.css';
+import Peer from 'simple-peer';
+import { io } from 'socket.io-client';
+
+const socket = io.connect('http://localhost:9999');
+
+function App() {
+  const [me, setMe] = useState("");
+  const [stream, setStream] = useState();
+  const [receivingCall, setReceivingCall] = useState(false);
+  const [caller, setCaller] = useState("");
+  const [name, setName] = useState("");
+  const [callerSignal, setCallerSignal] = useState();
+  const [callAccepted, setCallAccepted] = useState(false);
+  const [callEnded, setCallEnded] = useState(false);
+  const [idToCall, setIdToCall] = useState("");
+
+  const myVideo = useRef();
+  const userVideo = useRef();
+  const connectionRef = useRef();
+
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: true,
+        audio: true,
+      })
+      .then((stream) => {
+        setStream(stream);
+        console.log(myVideo.current);
+        if (myVideo.current) myVideo.current.srcObject = stream;
+      })
+      .catch((error) => {
+        console.error("Error accessing media devices:", error);
+      });
+
+    socket.on('me', (id) => {
+      setMe(id);
+      console.log(`me ${id}`);
+    });
+
+    socket.on('call_user', (data) => {
+      setReceivingCall(true);
+      setCaller(data.from);
+      setName(data.name);
+      setCallerSignal(data.signal);
+    });
+
+    // eslint-disable-next-line
+  }, []);
+
+  const callUser = (id) => {
+    const peer = new Peer({
+      initiator: true,
+      trickle: false,
+      stream: stream
+    });
+
+    peer.on("signal", (data) => {
+      socket.emit("call_user", {
+        userToCall: id,
+        signalData: data,
+        from: me,
+        name: name
+      });
+    });
+
+    peer.on("stream", (stream) => {
+      userVideo.current.srcObject = stream
+    });
+
+    socket.on("call_accepted", (signal) => {
+      setCallAccepted(true);
+      peer.signal(signal);
+    });
+
+    connectionRef.current = peer;
+  }
+
+  const answerCall = () => {
+    setCallAccepted(true);
+    const peer = new Peer({
+      initiator: false,
+      trickle: false,
+      stream: stream
+    });
+
+    peer.on("signal", (data) => {
+      socket.emit("answer_call", { signal: data, to: caller });
+    });
+
+    peer.on("stream", (stream) => {
+      userVideo.current.srcObject = stream
+    });
+
+    peer.signal(callerSignal);
+    connectionRef.current = peer;
+  }
+
+  const leaveCall = () => {
+    setCallEnded(true);
+    connectionRef.current.destroy();
+  }
+
+  return (
+    <>
+      <div className="App" style={{ display: 'flex', justifyContent: 'center' }}>
+        <h1>Video call App</h1>
+      </div>
+
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <div style={{marginRight: '2rem'}}>
+          
+            <video playsInline muted ref={myVideo} autoPlay style={{ width: '300px' }} />
+          
+        </div>
+        <div style={{marginLeft: '2rem'}}>
+          {
+            callAccepted && !callEnded ?
+              <video ref={userVideo} autoPlay playsInline style={{ width: '300px' }} />
+              : null
+          }
+        </div>
+      </div>
+
+      <div style={{ margin: '5rem' }}>
+        <div style={{ display: 'flex', paddingLeft: '1rem' }}>
+          <label htmlFor="name">Name </label>
+          <input id='name' type="text" value={name} onChange={(e) => setName(e.target.value)} />
+
+          <label htmlFor="idToCall">idToCall </label>
+          <input id='idToCall' type="text" value={idToCall} onChange={(e) => setIdToCall(e.target.value)} />
+        </div>
+
+        <div>
+          {
+            callAccepted && !callEnded ? (
+              <button onClick={leaveCall} >End Call</button>
+            ) : (
+              <button onClick={() => callUser(idToCall)}>Make Call</button>
+            )
+          }
+          {idToCall}
+        </div>
+      </div>
+
+      <div>
+        {
+          receivingCall && !callAccepted ? (
+            <div>
+              <h4>
+                {name} is calling...
+              </h4>
+              <button onClick={answerCall} style={{
+                backgroundColor: 'green'
+              }}>Answer</button>
+            </div>
+          ) : (
+            null
+          )
+        }
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,41 @@
+const express = require ('express');
+const app = express();
+const cors = require('cors');
+const http = require('http');
+const {Server} = require('socket.io');
+const PORT = 9999;
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST']
+    }
+});
+
+io.on('connection', (socket)=>{
+    console.log(`socket connected to id : ${socket.id}`);
+
+    // socket connection established
+    socket.emit('me', socket.id);
+
+    socket.on('disconnect', ()=>{
+        socket.broadcast.emit("call_ended");
+    })
+
+    socket.on('call_user', (data)=>{
+        console.log(`received data on call user: ${data}`);
+        io.to(data.userToCall).emit("call_user", {signal: data.signalData, from: data.from, name: data.name});
+    });
+
+    socket.on('answer_call', (data)=>{
+        io.to(data.to).emit("call_accepted", data.signal);
+    });
+});
+
+server.listen(PORT, ()=>{
+    console.log(`server connected to ${PORT}`);
+});
